af0a14d3a110066f54bed6fe542bc673d06d489e
ashutosh1919 <ashutoshhathidara98@gmail.com>
2019-10-07 14:14:51 +0530

    Added Tesseract

---
+++ generate_files.py
@@ -0,0 +1,3 @@
+from pdf_to_png import *
+
+generate_images_and_text_from_pdf("sumit/sumit.pdf","sumit")
---
+++ pdf_to_png.py
@@ -0,0 +1,30 @@
+from PIL import Image 
+import pytesseract 
+import sys 
+from pdf2image import convert_from_path 
+import os 
+
+pytesseract.pytesseract.tesseract_cmd = "C:\\Users\\Ashutosh\\AppData\\Local\\Tesseract-OCR\\tesseract.exe"
+
+def generate_images_and_text_from_pdf(PDF_file,target_dir):
+    pages = convert_from_path(PDF_file, 500)
+    image_counter = 1
+
+    for page in pages:
+        filename = target_dir+"/page_"+str(image_counter)+".jpg"
+        page.save(filename, 'JPEG') 
+        image_counter = image_counter + 1
+
+    filelimit = image_counter-1
+
+    outfile = target_dir+"/out_text.txt"
+
+    f = open(outfile, "w") 
+
+    for i in range(1, filelimit + 1): 
+        filename = target_dir+"/page_"+str(i)+".jpg"
+        text = str(((pytesseract.image_to_string(Image.open(filename)))))
+        text = text.replace('-\n', '') 
+        f.write(text) 
+
+    f.close()
